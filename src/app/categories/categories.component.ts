ea22647af61dd5f13345eee9e8aafb2680e0ab2f
Iryna Bondarenko <ira.bondarenko@ukr.net>
2024-12-09 13:31:26 +0000

    created categories collection in firestore & added form validation

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Firestore, collection, addDoc } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './categories.component.html',
-  styleUrl: './categories.component.css'
+  styleUrl: './categories.component.css',
 })
 export class CategoriesComponent {
+  constructor(private firestore: Firestore) {}
 
+  async onSubmit(formData: any): Promise<void> {
+    try {
+      const categoryData = formData.value;
+
+      const categoriesCollection = collection(this.firestore, 'categories');
+
+      const docRef = await addDoc(categoriesCollection, categoryData);
+
+      console.log('Category added successfully!', docRef.id);
+    } catch (error) {
+      console.error('Error adding category: ', error);
+    }
+  }
 }
