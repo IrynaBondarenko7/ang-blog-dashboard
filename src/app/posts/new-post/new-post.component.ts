4bac9eee0858437063c142ada64538c9fce14499
Iryna Bondarenko <ira.bondarenko@ukr.net>
2025-03-31 20:28:31 +0100

    created permalink

--- src/app/posts/new-post/new-post.component.ts
+++ src/app/posts/new-post/new-post.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-post',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './new-post.component.html',
-  styleUrl: './new-post.component.css'
+  styleUrl: './new-post.component.css',
 })
 export class NewPostComponent {
+  permalink = new FormControl({ value: '', disabled: true });
 
+  onTitleChanged($event: Event) {
+    const title = ($event.target as HTMLInputElement).value;
+    const formattedTitle = title.trim().toLowerCase().replace(/\s+/g, '-');
+
+    this.permalink.setValue(formattedTitle);
+  }
 }
